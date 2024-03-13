6c99e79642f109e1b4e953ee206d33f32f0f80c1
ibrahimfares511 <ibrahimfares511@gmail.com>
2024-03-13 23:26:35 +0200

    main section

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 "use strict";
-// All Variables
+// ============ All Variables ============
 const menuToggleBtn = document.querySelector("#menu__toggle__btn");
 const menuBtn = document.querySelector("#menu__btns");
+const mainNavbar = document.querySelector("#main__navbar");
+const myBarIndicator = document.getElementById("myBar");
+// ============ All Variables ============
 
-// All Functions
-const handelClickOnMenuBtn = (e) => {
+// ============ All Functions ============
+// Toggle Navbar Menu FOr Mobile
+const toggleNavbarMenu = (e) => {
 	const thisBtn = e.target.closest("button");
 
 	thisBtn.classList.toggle("opened");
@@ -12,5 +16,24 @@ const handelClickOnMenuBtn = (e) => {
 	menuBtn.classList.toggle("opened");
 };
 
-// Handel All Function
-menuToggleBtn.addEventListener("click", handelClickOnMenuBtn);
+// Scroll Window
+const scrollWindow = (e) => {
+	let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	let height =
+		document.documentElement.scrollHeight -
+		document.documentElement.clientHeight;
+	let scrolled = (winScroll / height) * 100;
+
+	myBarIndicator.style.width = scrolled + "%";
+	if (document.documentElement.scrollTop >= 100) {
+		mainNavbar.classList.add("scrolling");
+	} else {
+		mainNavbar.classList.remove("scrolling");
+	}
+};
+// ============ All Functions ============
+
+// ============ Handel All Function ============
+menuToggleBtn.addEventListener("click", toggleNavbarMenu);
+window.addEventListener("scroll", scrollWindow);
+// ============ Handel All Function ============
