9f43c273451fa765e5c7663a761a668a95fc22a0
ibrahimfares511 <ibrahimfares511@gmail.com>
2024-05-22 00:51:11 +0300

    fix Some Issues home Page And Book Details Page

--- js/main.js
+++ js/main.js
@@ -9,56 +9,59 @@ const themeButton = document.querySelector("#theme__button");
 const mainImage = document.querySelector(".main-image");
 const localTheme = window.localStorage.getItem("mrs-eman-theme") || "light";
 const FeaturesCardsTitle = document.querySelectorAll(".features_card_title");
+const coursesCard = document.querySelectorAll(".course-card");
+const sectionTitle = document.querySelectorAll(".section-title");
+const featuresCardTitle = document.querySelectorAll(".features_card_title");
 // ============ All Variables ============
 
 // ============ All Functions ============
-// Toggle Navbar Menu FOr Mobile
+// Toggle Navbar Menu For Mobile
 const toggleNavbarMenu = (e) => {
-	const thisBtn = e.target.closest("button");
+  const thisBtn = e.target.closest("button");
 
-	thisBtn.classList.toggle("opened");
-	thisBtn.setAttribute("aria-expanded", thisBtn.classList.contains("opened"));
-	menuBtn.classList.toggle("opened");
+  thisBtn.classList.toggle("opened");
+  thisBtn.setAttribute("aria-expanded", thisBtn.classList.contains("opened"));
+  menuBtn.classList.toggle("opened");
 };
 
 // Scroll Window
 const scrollWindow = (e) => {
-	let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-	let height =
-		document.documentElement.scrollHeight -
-		document.documentElement.clientHeight;
-	let scrolled = (winScroll / height) * 100;
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
 
-	myBarIndicator.style.width = scrolled + "%";
-	if (document.documentElement.scrollTop >= 100) {
-		mainNavbar.classList.add("scrolling");
-	} else {
-		mainNavbar.classList.remove("scrolling");
-	}
+  myBarIndicator.style.width = scrolled + "%";
+  if (document.documentElement.scrollTop >= 100) {
+    mainNavbar.classList.add("scrolling");
+  } else {
+    mainNavbar.classList.remove("scrolling");
+  }
 };
 
 // Toggle Theme
 const toggleTheme = (e) => {
-	let myButton = e.target.closest("button");
+  let myButton = e.target.closest("button");
 
-	myButton.dataset.theme = myButton.dataset.theme === "dark" ? "light" : "dark";
-	document.body.dataset.theme = myButton.dataset.theme;
-	window.localStorage.setItem("mrs-eman-theme", myButton.dataset.theme);
+  myButton.dataset.theme = myButton.dataset.theme === "dark" ? "light" : "dark";
+  document.body.dataset.theme = myButton.dataset.theme;
+  window.localStorage.setItem("mrs-eman-theme", myButton.dataset.theme);
 };
 
 const checkTheme = () => {
-	themeButton.dataset.theme = localTheme;
-	document.body.dataset.theme = localTheme;
+  themeButton.dataset.theme = localTheme;
+  document.body.dataset.theme = localTheme;
 };
 
 // Move Image When Mouse Move
 const moveImage = (e) => {
-	let h = header.clientHeight;
-	let w = header.clientWidth;
-	let i = (10 * e.pageX) / h - 1;
-	let o = (2 * e.pageY) / w - 1;
-	let s = 20 * o;
-	mainImage.style.transform = `translate(${i}px, ${s}px)`;
+  let h = header.clientHeight;
+  let w = header.clientWidth;
+  let i = (10 * e.pageX) / h - 1;
+  let o = (2 * e.pageY) / w - 1;
+  let s = 20 * o;
+  mainImage.style.transform = `translate(${i}px, ${s}px)`;
 };
 
 // ============ All Functions ============
@@ -67,7 +70,7 @@ const moveImage = (e) => {
 menuToggleBtn.addEventListener("click", toggleNavbarMenu);
 themeButton.addEventListener("click", toggleTheme);
 window.addEventListener("scroll", () => {
-	scrollWindow();
+  scrollWindow();
 });
 header.addEventListener("mousemove", moveImage);
 checkTheme();
@@ -75,24 +78,67 @@ scrollWindow();
 
 // ============ Handel All Function ============
 
-let coursesCard = document.querySelectorAll(".course-card");
-
+// Make Animation With Mouse Move In Course Card
 coursesCard.forEach((card) => {
-	card.addEventListener("mousemove", function (e) {
-		let limits = 15;
-		let rect = card.getBoundingClientRect();
-		let x = e.clientX - rect.left;
-		let y = e.clientY - rect.top;
-		let offsetX = x / rect.width;
-		let offsetY = y / rect.height;
+  card.addEventListener("mousemove", function (e) {
+    let limits = 15;
+    let rect = card.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    let offsetX = x / rect.width;
+    let offsetY = y / rect.height;
 
-		let rotateY = offsetX * (limits * 2) - limits;
-		let rotateX = offsetY * (limits * 2) - limits;
+    let rotateY = offsetX * (limits * 2) - limits;
+    let rotateX = offsetY * (limits * 2) - limits;
 
-		card.style.transform = `rotateY(${rotateY}deg) rotateX(${-rotateX}deg)`;
-	});
+    card.style.transform = `rotateY(${rotateY}deg) rotateX(${-rotateX}deg)`;
+  });
 
-	card.addEventListener("mouseleave", function (e) {
-		card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-	});
+  card.addEventListener("mouseleave", function (e) {
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  });
+});
+
+// Make Animation Title With Scroll
+window.addEventListener("scroll", function () {
+  sectionTitle.forEach((title) => {
+    const titleOffsetTop = title.offsetTop;
+    const scrollPosition = window.scrollY + window.innerHeight / 2.4;
+
+    let newRight = -100 + (scrollPosition - titleOffsetTop);
+
+    // تأكد من أن العنصر لا يتحرك بعد الوصول إلى موضعه النهائي
+    if (newRight > 0) {
+      newRight = 0;
+    }
+
+    title.style.setProperty("--title-after-right", `${newRight}px`);
+  });
+
+  featuresCardTitle.forEach((title) => {
+    const titleOffsetTop = title.offsetTop;
+    const scrollPosition = window.scrollY + window.innerHeight / 10.3;
+    const color = title.dataset.background;
+
+    let newY = -400 + (scrollPosition - titleOffsetTop);
+    let newRotate =
+      30 -
+      ((scrollPosition - titleOffsetTop) / (window.innerHeight / 2.3)) * 20;
+
+    // تأكد من أن العنصر لا يتحرك بعد الوصول إلى موضعه النهائي
+    if (newY > 0) {
+      newY = 0;
+    } else if (newY < -350) {
+      newY = -350;
+    }
+
+    // تأكد من أن الدوران لا يتجاوز 0 درجة
+    if (newRotate < 0) {
+      newRotate = 0;
+    }
+
+    title.style.setProperty("--title-translate-y", `${newY}px`);
+    title.style.setProperty("--title-rotate", `${newRotate}deg`);
+    title.style.backgroundColor = color;
+  });
 });
